ccdae74d1fc2ba1c5a0ea3b8135309504c35ebef
shizunari <mare.fe.2006+biz@gmail.com>
2026-04-09 17:03:11 +0900

    指摘箇所を修正したのでコミットする。
    ・不要なコメントの削除
    ・エラーメッセージを標準出力から標準エラー出力に変更
    ・変数の名称を見直し
    ・文字列の足し合わせを見直し、式展開に変更
    ・曜日確認処理をcwdayからwdayに変更
    ・カレンダーの日付処理を見直し（while処理の取りやめ）
    ・引数に値を指定しなかった場合のエラー処理を追加

--- 02.calendar/calender_program.rb
+++ 02.calendar/calender_program.rb
@@ -5,68 +5,55 @@ require 'optparse'
 require 'date'
 
 # 引数の設定と判定処理
-params = {}
+input_options = {:m => Date.today.month.to_i, :y => Date.today.year.to_i }
 OptionParser.new do |opt|
   opt.on('-m', '--month MONTH', '出力する月を数字で指定してください') do |month|
-    unless 0 < month.to_i && month.to_i < 13
-      puts '１〜１２の範囲で月を指定してください。'
-      exit
+    unless (1..12).include?(month.to_i)
+      abort 'Err: １〜１２の範囲で月数を指定してください。'
     end
-    params[:m] = month.to_i
+    input_options[:m] = month.to_i
   end
 
   opt.on('-y', '--year YEAR', '出力する西暦を数字で指定してください') do |year|
     if year.to_i < 1970
-      puts '1970年以降の年数を指定してください。'
-      exit
+      abort 'Err: 1970年以降の年数を指定してください。'
     end
     unless year.to_s.length == 4
-      puts '年数は西暦で入力してください。'
-      exit
+      abort 'Err: 年数は4桁の西暦で入力してください。'
     end
-    params[:y] = year.to_i
+    input_options[:y] = year.to_i
   end
 
-  opt.parse!(ARGV)
+  begin
+    opt.parse!(ARGV)
+  rescue
+    # 引数を指定するが値を入力しなかった場合にエラーとなる為、エラー処理
+    abort "Err: 引数を使用する場合は、値を設定してください。"
+  end
 end
 
-# 引数を設定しなかった時の処理（暫定）
-input_year = params[:y]
-input_year = Date.today.year.to_i if input_year.nil?
-
-input_month = params[:m]
-input_month = Date.today.month.to_i if input_month.nil?
+# 変数に入力された月と年をセット
+input_month = input_options[:m]
+input_year = input_options[:y]
 
 # 年・月・日・曜日をセット
 Week_str = '日 月 火 水 木 金 土'
 month_start = Date.new(input_year, input_month, 1)
-month_end = Date.new(input_year, input_month, -1) # これでもいいらしい
-calender_head = input_month.to_s + '月 ' + input_year.to_s + '年'
-start_point = month_start.cwday
-days_str = ''
+month_end = Date.new(input_year, input_month, -1)
+calender_head = "#{input_month}月 #{input_year}年"
 
-# カレンダーの出力値作成
 # 日付のスタート位置調整
-unless start_point == 7
-  start_point.times do
-    days_str += '   '
-  end
-end
+days_str = '   ' * month_start.wday
+
 # 日付の出力位置調整
-ii = 0
-while month_end.day > ii
-  ii += 1
-  days_str = if ii < 10
-               days_str + ' ' + ii.to_s
-             else
-               days_str + ii.to_s
-             end
-  xx = Date.parse(input_year.to_s + '-' + input_month.to_s + '-' + ii.to_s)
-  days_str = if xx.saturday?
-               days_str + "\n"
-             else
-               days_str + ' '
-             end
+(month_start.day..month_end.day).each do |date|
+  if (1..9).include?(date)
+    days_str += " #{date}".ljust(3)
+  else
+    days_str += "#{date}".ljust(3)
+  end
+  check_saturday = Date.parse("#{input_year}-#{input_month}-#{date}")
+  days_str = days_str + "\n" if check_saturday.saturday?
 end
 
 # 結果出力
