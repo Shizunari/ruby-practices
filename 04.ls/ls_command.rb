0bb22311bbee9b09b19dbb9aa7af4c823316fa4e
shizunari <mare.fe.2006+biz@gmail.com>
2026-04-27 14:55:04 +0900

    変数名と構造の見直し

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -1,36 +1,32 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def get_word_length(words)
-  words.map(&:length).max
+# 出力列の最大値を指定する
+COLMUN_UPPER_LIMIT = 3
+
+def align_array(filenames, colmun)
+  rows_count = filenames.length.ceildiv(colmun)
+  align_sorted_array = filenames.each_slice(rows_count).to_a
+  align_sorted_array[0].zip(*align_sorted_array[1..])
 end
 
-def alignment(words, colmun)
-  array_rows = (words.length.to_f / colmun).ceil
-  sort_array = words.sort_by(&:downcase)
-  align_sort_array = sort_array.each_slice(array_rows).to_a
-  align_sort_array[0].zip(*align_sort_array[1..])
-end
-
-def output_word_only(words, max_length)
-  words.each do |row_words|
-    row_words.each_with_index do |word, i|
-      if i == row_words.length - 1
-        print word
+def to_matrix(filenames, max_length)
+  filenames.each do |row_filenames|
+    row_filenames.each_with_index do |filename, i|
+      if i == row_filenames.length - 1
+        print filename
       else
-        print word.to_s.ljust(max_length + 4)
+        print filename.to_s.ljust(max_length + 4)
       end
     end
     puts
   end
 end
 
-# 出力列の最大値を設定する
-SET_OUTPUT_COLMUN = 3
-
 files_info = Dir.glob('*')
 exit if files_info[0].nil?
 
-max_word_len = get_word_length(files_info)
-align_files_info = alignment(files_info, SET_OUTPUT_COLMUN)
-output_word_only(align_files_info, max_word_len)
+max_filename_length = files_info.map(&:length).max
+sorted_filenames = files_info.sort_by(&:downcase)
+aligned_filenames = align_array(sorted_filenames, COLMUN_UPPER_LIMIT)
+to_matrix(aligned_filenames, max_filename_length)
