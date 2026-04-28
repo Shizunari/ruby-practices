9f671711dbd95ab00fd4bd0e0e47c1ab5da96722
shizunari <mare.fe.2006+biz@gmail.com>
2026-04-28 14:28:35 +0900

    各部名称の見直し（３回目）

--- 04.ls/ls_command.rb
+++ 04.ls/ls_command.rb
@@ -4,13 +4,13 @@
 # 出力列の最大値を指定する
 COLMUN_UPPER_LIMIT = 3
 
-def align_array(filenames, colmun)
+def array_to_matrix(filenames, colmun)
   rows_count = filenames.length.ceildiv(colmun)
-  align_sorted_array = filenames.each_slice(rows_count).to_a
-  align_sorted_array[0].zip(*align_sorted_array[1..])
+  slice_filenames_array = filenames.each_slice(rows_count).to_a
+  slice_filenames_array[0].zip(*slice_filenames_array[1..])
 end
 
-def to_matrix(filenames, max_length)
+def display_matrix(filenames, max_length)
   filenames.each do |row_filenames|
     row_filenames.each_with_index do |filename, i|
       if i == row_filenames.length - 1
@@ -28,5 +28,5 @@ exit if files_info[0].nil?
 
 max_filename_length = files_info.map(&:length).max
 sorted_filenames = files_info.sort_by(&:downcase)
-aligned_filenames = align_array(sorted_filenames, COLMUN_UPPER_LIMIT)
-to_matrix(aligned_filenames, max_filename_length)
+matrixed_filenames = array_to_matrix(sorted_filenames, COLMUN_UPPER_LIMIT)
+display_matrix(matrixed_filenames, max_filename_length)
